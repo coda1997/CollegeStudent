ed827d20d52a9e436b18ef57751adb9fff191bdf
Overlooker <Overlooker1997@gmail.com>
2016-12-09 23:45:20 +0800

    update with implementing qustion content

--- app/src/main/java/com/example/a60440/collegestudent/activity/VideoActivity.java
+++ app/src/main/java/com/example/a60440/collegestudent/activity/VideoActivity.java
@@ -2,26 +2,21 @@ package com.example.a60440.collegestudent.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.VideoView;
 
 import com.example.a60440.collegestudent.R;
 
-/**
- * Created by 60440 on 2016/11/29.
- */
+import butterknife.BindView;
 
-public class VideoActivity extends Activity {
-    private VideoView videoView;
+public class VideoActivity extends Activity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vedio_main);
-        InitView();
+        initView();
     }
 
-    private void InitView() {
-        videoView = (VideoView)findViewById(R.id.id_videoView_main);
+    private void initView() {
 
     }
-
 }
